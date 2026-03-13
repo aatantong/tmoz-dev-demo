41aa002b1a27f21e728ab60b63e2a239aa289729
Raffy <atantong@deloitte.com>
2026-03-13 11:44:20 +0800

    changed headless config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 use: {
-    headless: false,        // browser visible
+    headless: process.env.CI ? true : false,     // browser visible
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
